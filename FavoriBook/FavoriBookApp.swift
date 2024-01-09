d2f6e92a6871157de163b9b93f95ab3e99003267
alperenkavukk <kavuk.67.76@gmail.com>
2024-01-09 11:47:20 +0300

    Initial Commit

---
+++ FavoriBook/FavoriBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriBookApp.swift
+//  FavoriBook
+//
+//  Created by Alperen Kavuk on 9.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct FavoriBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
